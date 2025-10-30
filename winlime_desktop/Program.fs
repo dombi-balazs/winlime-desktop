6c9d944aba35cb185183c4dfd972dcd8470fad8f
Dombi Balazs Sandor <dombi.balazs.sandor@gmail.com>
2025-10-30 21:06:37 +0100

    initial commit

---
+++ winlime_desktop/Program.fs
@@ -0,0 +1,18 @@
+﻿namespace winlime_desktop
+
+open System
+open Avalonia
+
+module Program =
+
+    [<CompiledName "BuildAvaloniaApp">]
+    let buildAvaloniaApp () =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace(areas = Array.empty)
+
+    [<EntryPoint; STAThread>]
+    let main argv =
+        buildAvaloniaApp().StartWithClassicDesktopLifetime(argv)
